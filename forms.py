2f366efd07fae24c3ec70c449b9fd0e10ec4aeea
hb2atx <josephkillelea@gmail.com>
2024-03-07 20:50:24 -0600

    l

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from wtforms.validators import DataRequired,EqualTo, Length, ValidationError
 from models import User
 
 class LoginForm(FlaskForm):
